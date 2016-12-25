ebe8f126f04cf5b5895692c59222bf9d6445e6b3
Mikhail <neon-pf@yandex.ru>
2016-12-25 23:14:03 +0300

    make trees great again

--- src/ru/mail/polis/Main.java
+++ src/ru/mail/polis/Main.java
@@ -1,19 +1,24 @@
 package ru.mail.polis;
 
+import java.util.TreeSet;
+
 public class Main {
 
     public static void main(String[] args) {
-        RedBlackTree<Integer> set = new RedBlackTree<>();
-        System.out.println(set.add(5));
-        System.out.println(set.add(6));
-        System.out.println(set.add(7));
-        System.out.println(set.add(8));
-        System.out.println();
-        System.out.println(set.remove(5));
-        System.out.println(set.remove(6));
-        System.out.println(set.remove(8));
-        System.out.println();
-        System.out.println(set.add(10));
-        System.out.println(set.remove(10));
+
+        TreeSet<Integer> OK = new TreeSet<>();
+        ISortedSet<Integer> set = new AVLTree<>();
+        //ISortedSet<Integer> set = new RedBlackTree<>();
+        for (int value = 0; value < 10; value++) {
+            assert OK.add(value) == set.add(value);
+        }
+        assert OK.last().equals(set.last());
+        for (int value = 10; value >= 0; value--) {
+            assert OK.remove(value) == set.remove(value);
+            assert OK.remove(value) == set.remove(value);
+            if (!OK.isEmpty()) {
+                assert OK.first().equals(set.first());
+            }
+        }
     }
 }
